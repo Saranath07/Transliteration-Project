629000f67a8534f1794d94d9db0363f15b3b42db
Saranath07 <saranathp34@gmail.com>
2024-09-29 22:28:19 +0530

    First commit

--- llama405.py
+++ llama405.py
@@ -1,9 +1,14 @@
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 client = ChatNVIDIA(
   model="meta/llama-3.1-405b-instruct",
-  api_key="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", 
+  api_key=os.getenv("NVIDEA_API_KEY"), 
   temperature=0.2,
   top_p=0.7,
   max_tokens=1024,
